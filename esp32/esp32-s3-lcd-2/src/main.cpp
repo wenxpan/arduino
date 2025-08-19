207a9c0c2158d5f9d1bb26889841e158fd1724de
wenxpan <panwenxuan42@gmail.com>
2025-08-19 21:31:06 +1000

    rotate images

--- esp32/esp32-s3-lcd-2/src/main.cpp
+++ esp32/esp32-s3-lcd-2/src/main.cpp
@@ -9,9 +9,13 @@
 #include <SD_MMC.h>
 #include "./display/JpegFunc.h"
 
-#define JPEG_FILENAME "/tvsignal.jpg"
-
 Arduino_GFX *gfx = nullptr;
+File root;
+String imageFiles[50]; // Array to hold image filenames
+int imageCount = 0;
+int currentImage = 0;
+unsigned long lastChangeTime = 0;
+const unsigned long rotationInterval = 5000; // 5 seconds
 
 // pixel drawing callback
 static int jpegDrawCallback(JPEGDRAW *pDraw)
@@ -19,6 +23,37 @@ static int jpegDrawCallback(JPEGDRAW *pDraw)
   // USBSerial.printf("Draw pos = %d,%d. size = %d x %d\n", pDraw->x, pDraw->y, pDraw->iWidth, pDraw->iHeight);
   gfx->draw16bitBeRGBBitmap(pDraw->x, pDraw->y, pDraw->pPixels, pDraw->iWidth, pDraw->iHeight);
   return 1;
+}
+void loadImageList()
+{
+  root = SD.open("/");
+  File file = root.openNextFile();
+  imageCount = 0;
+
+  while (file && imageCount < 50)
+  {
+    if (!file.isDirectory())
+    {
+      String filename = file.name();
+      // Filter hidden files
+      if ((filename.endsWith(".jpg") || filename.endsWith(".jpeg")) &&
+          !filename.startsWith("._") &&
+          !filename.startsWith("."))
+      {
+        // Store with full path
+        imageFiles[imageCount] = "/" + filename;
+        USBSerial.println("Found valid image: " + imageFiles[imageCount]);
+        imageCount++;
+      }
+      else
+      {
+        USBSerial.println("Skipping: " + filename);
+      }
+    }
+    file = root.openNextFile();
+  }
+
+  USBSerial.printf("Found %d valid images\n", imageCount);
 }
 
 void setup(void)
@@ -30,16 +65,45 @@ void setup(void)
   gfx = setupGfx();
 
   // drawExamplePepper(gfx);
-
   setupSD();
-
   logSDInfo();
-  drawJpegFromSD(gfx, JPEG_FILENAME, jpegDrawCallback);
 
-  delay(5 * 1000);
+  // drawJpegFromSD(gfx, JPEG_FILENAME, jpegDrawCallback);
+
+  // Load list of JPEG images
+  loadImageList();
+
+  // Display first image if available
+  if (imageCount > 0)
+  {
+    drawJpegFromSD(gfx, imageFiles[currentImage].c_str(), jpegDrawCallback);
+    lastChangeTime = millis();
+  }
+  else
+  {
+    USBSerial.println("No JPEG images found!");
+  }
 }
 
 void loop()
 {
-  delay(10 * 1000);
+  if (imageCount > 0)
+  {
+    unsigned long currentTime = millis();
+
+    // Check if it's time to change image
+    if (currentTime - lastChangeTime >= rotationInterval)
+    {
+      lastChangeTime = currentTime;
+
+      // Move to next image
+      currentImage = (currentImage + 1) % imageCount;
+
+      // Display the image
+      USBSerial.println("Displaying: " + imageFiles[currentImage]);
+      drawJpegFromSD(gfx, imageFiles[currentImage].c_str(), jpegDrawCallback);
+    }
+  }
+
+  delay(1 * 1000);
 }
