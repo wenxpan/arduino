ae3cdb0982e36dcbb58533827ea090cdb57132c1
wenxpan <panwenxuan42@gmail.com>
2025-09-28 17:09:26 +1000

    add button 2

--- mini-display/esp-32d/src/main.cpp
+++ mini-display/esp-32d/src/main.cpp
@@ -1,25 +1,28 @@
 #include <Arduino.h>
 #include <ezButton.h>
 #include <WiFi.h>
-#include <HTTPClient.h>
+#include <AsyncTCP.h>
 #include "./secrets.h"
 
-#define BUTTON_PIN 18
+#define BUTTON_1_PIN 18
+#define BUTTON_2_PIN 22
 #define LED_PIN 21
 
-ezButton button(BUTTON_PIN);
-
+ezButton button1(BUTTON_1_PIN);
+ezButton button2(BUTTON_2_PIN);
 int led_state = LOW;
-bool shouldSendRequest = false;
-unsigned long requestStartTime = 0;
-bool requestInProgress = false;
 static bool sendRain = true;
+
+// Async HTTP variables
+AsyncClient *client = nullptr;
+bool requestPending = false;
 
 void setup()
 {
   Serial.begin(9600);
   pinMode(LED_PIN, OUTPUT);
-  button.setDebounceTime(50);
+  button1.setDebounceTime(50);
+  button2.setDebounceTime(50);
 
   WiFi.mode(WIFI_STA);
   WiFi.begin(ESP_SSID, ESP_PASS);
@@ -36,64 +39,141 @@ void setup()
   digitalWrite(LED_PIN, HIGH);
 }
 
-void sendRequest(const char *path)
+void onData(void *arg, AsyncClient *client, void *data, size_t len)
 {
-  if (WiFi.status() == WL_CONNECTED)
+  Serial.printf("Data received: ");
+  Serial.write((uint8_t *)data, len);
+  Serial.println();
+}
+
+void onConnect(void *arg, AsyncClient *client)
+{
+  Serial.println("Connected to server");
+
+  // Get the path from the argument
+  String *path = (String *)arg;
+
+  // Send HTTP GET request with dynamic path
+  String request = "GET " + *path + " HTTP/1.1\r\n";
+  request += "Host: 192.168.4.1\r\n";
+  request += "Connection: close\r\n\r\n";
+
+  Serial.println("Sending request: " + request);
+  client->write(request.c_str());
+}
+
+void onDisconnect(void *arg, AsyncClient *client)
+{
+  Serial.println("Disconnected from server");
+  requestPending = false;
+
+  // Clean up the allocated path string
+  if (arg)
   {
-    HTTPClient http;
-    String url = String("http://192.168.4.1") + path;
-    http.begin(url);
-    int httpCode = http.GET();
-    if (httpCode > 0)
-    {
-      Serial.printf("GET %s response: %d\n", path, httpCode);
-      String payload = http.getString();
-      Serial.println(payload);
-    }
-    else
-    {
-      Serial.printf("GET %s failed, error: %s\n", path, http.errorToString(httpCode).c_str());
-    }
-    http.end();
+    delete (String *)arg;
   }
-  else
+
+  if (client)
+  {
+    delete client;
+    client = nullptr;
+  }
+}
+
+void onError(void *arg, AsyncClient *client, int error)
+{
+  Serial.printf("Connection error: %d\n", error);
+  requestPending = false;
+
+  // Clean up the allocated path string
+  if (arg)
+  {
+    delete (String *)arg;
+  }
+
+  if (client)
+  {
+    delete client;
+    client = nullptr;
+  }
+}
+
+void onTimeout(void *arg, AsyncClient *client, uint32_t time)
+{
+  Serial.printf("Connection timeout: %u\n", time);
+  requestPending = false;
+
+  // Clean up the allocated path string
+  if (arg)
+  {
+    delete (String *)arg;
+  }
+
+  if (client)
+  {
+    delete client;
+    client = nullptr;
+  }
+}
+
+void sendAsyncRequest(const char *path)
+{
+  if (WiFi.status() != WL_CONNECTED)
   {
     Serial.println("WiFi not connected");
+    return;
+  }
+
+  if (requestPending)
+  {
+    Serial.println("Request already in progress");
+    return;
+  }
+
+  client = new AsyncClient();
+  if (!client)
+  {
+    Serial.println("Failed to create client");
+    return;
+  }
+
+  // Allocate a string to hold the path
+  String *pathPtr = new String(path);
+
+  requestPending = true;
+
+  client->onData(&onData, pathPtr);
+  client->onConnect(&onConnect, pathPtr);
+  client->onDisconnect(&onDisconnect, pathPtr);
+  client->onError(&onError, pathPtr);
+  client->onTimeout(&onTimeout, pathPtr);
+
+  if (!client->connect("192.168.4.1", 80))
+  {
+    Serial.println("Connect failed");
+    delete client;
+    delete pathPtr; // Clean up on failure
+    client = nullptr;
+    requestPending = false;
   }
 }
 
 void loop()
 {
-  button.loop();
-  if (button.isPressed() && !requestInProgress)
-  {
-    Serial.println("The button is pressed");
+  button1.loop();
+  button2.loop();
 
-    shouldSendRequest = true;
+  if (button1.isPressed() && !requestPending)
+  {
+    Serial.println("The button1 is pressed");
+    Serial.println("Send rain request");
+    sendAsyncRequest("/rain");
   }
 
-  if (shouldSendRequest && !requestInProgress)
+  if (button2.isPressed() && !requestPending)
   {
-    digitalWrite(LED_PIN, LOW);
-    if (sendRain)
-    {
-      Serial.println("Send rain request");
-      sendRequest("/rain");
-    }
-    else
-    {
-      Serial.println("Send memories request");
-      sendRequest("/memories");
-    }
-    sendRain = !sendRain;
-    shouldSendRequest = false;
-    requestInProgress = true;
-    requestStartTime = millis();
-    digitalWrite(LED_PIN, HIGH);
-  }
-
-  if (requestInProgress && (millis() - requestStartTime > 1000))
-  {
-    requestInProgress = false;
+    Serial.println("The button2 is pressed");
+    Serial.println("Send memories request");
+    sendAsyncRequest("/memories");
   }
 }
