4c1ff30aa2e16b53b32bffd69e209f50df7033af
wenxpan <panwenxuan42@gmail.com>
2025-09-28 16:09:18 +1000

    add button

--- mini-display/esp-32d/src/main.cpp
+++ mini-display/esp-32d/src/main.cpp
@@ -1,22 +1,41 @@
 #include <Arduino.h>
+#include <ezButton.h>
 
-const int ledPin = 21;
+/*
+ * This ESP32 code is created by esp32io.com
+ *
+ * This ESP32 code is released in the public domain
+ *
+ * For more detail (instruction and wiring diagram), visit https://esp32io.com/tutorials/esp32-button-toggle-led
+ */
+
+#define BUTTON_PIN 18 // ESP32 pin GPIO18, which connected to button
+#define LED_PIN 21    // ESP32 pin GPIO21, which connected to led
+
+ezButton button(BUTTON_PIN); // create ezButton object that attach to pin 7;
+
+// variables will change:
+int led_state = LOW; // the current state of LED
 
 void setup()
 {
-  pinMode(ledPin, OUTPUT);
+  Serial.begin(9600);         // initialize serial
+  pinMode(LED_PIN, OUTPUT);   // set ESP32 pin to output mode
+  button.setDebounceTime(50); // set debounce time to 50 milliseconds
 }
 
 void loop()
 {
-  for (int brightness = 0; brightness <= 255; brightness++)
+  button.loop(); // MUST call the loop() function first
+
+  if (button.isPressed())
   {
-    analogWrite(ledPin, brightness);
-    delay(10);
-  }
-  for (int brightness = 255; brightness >= 0; brightness--)
-  {
-    analogWrite(ledPin, brightness);
-    delay(15);
+    Serial.println("The button is pressed");
+
+    // toggle state of LED
+    led_state = !led_state;
+
+    // control LED arccoding to the toggleed sate
+    digitalWrite(LED_PIN, led_state);
   }
 }
